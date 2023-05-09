7103f387200f4fb08813a54abd1a343fc91c5a55
n0k0m3 <10925298+n0k0m3@users.noreply.github.com>
2023-05-09 18:10:10 -0400

    feat(jinja): load context from json

--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
-import os
 import json
+import os
+
 
 def render(template_name, folder='templates', **kwargs):
     # Set up environment
@@ -10,25 +11,29 @@ def render(template_name, folder='templates', **kwargs):
     # Render template with data
     return template.render(**kwargs)
 
+
 def save(filename, content):
     with open(filename, 'w') as f:
         f.write(content)
 
+
 def main():
     # Get template
     template = 'user-data.jinja'
-    # Get context from environment
-    context = json.loads(os.environ['JINJA_CONFIG'])
+    # Get context from config.json
+    if os.path.exists('config.json'):
+        with open('config.json') as f:
+            context = json.loads(f.read())
+    else:
+        context = {
+            "username": "ubuntu",
+            "hashed_password": "$6$exDY1mhS4KUYCE/2$zmn9ToZwTKLhCw.b4/b.ZRTIZM30JZ4QrOQ2aOXJ8yk96xpcCof0kxKwuX1kqLG/ygbJ1f8wxED22bTL4F46P0",
+            "hostname": "ubuntu-server",
+        }
     # Render template with context
-    content = render(template, env=os.environ, **context)
+    content = render(template, **context)
     save('user-data', content)
 
+
 if __name__ == '__main__':
-    if os.environ["JINJA_CONFIG"] == "":
-        os.environ["JINJA_CONFIG"] = """{
-            "username": "ubuntu",
-            "hostname": "ubuntu-server",
-            "hashed_password": "$6$exDY1mhS4KUYCE/2$zmn9ToZwTKLhCw.b4/b.ZRTIZM30JZ4QrOQ2aOXJ8yk96xpcCof0kxKwuX1kqLG/ygbJ1f8wxED22bTL4F46P0"
-        }
-        """
     main()
